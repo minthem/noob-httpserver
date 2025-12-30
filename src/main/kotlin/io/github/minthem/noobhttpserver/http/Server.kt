93dbe32fb37e1b3bef8d6899d3fae3a7c74d8942
minthem <114272528+minthem@users.noreply.github.com>
2025-12-30 18:29:23 +0900

    Server側 実装(後で直す)

--- src/main/kotlin/io/github/minthem/noobhttpserver/http/Server.kt
+++ src/main/kotlin/io/github/minthem/noobhttpserver/http/Server.kt
@@ -1,14 +1,24 @@
 package io.github.minthem.noobhttpserver.http
 
+import io.github.minthem.noobhttpserver.router.Context
+import io.github.minthem.noobhttpserver.router.Handler
+import io.github.minthem.noobhttpserver.router.Router
 import java.net.InetSocketAddress
 import java.net.StandardProtocolFamily
 import java.nio.ByteBuffer
 import java.nio.channels.ServerSocketChannel
+import java.nio.channels.SocketChannel
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.Executors
 
 class Server(
     private val port: UShort
 ) {
+
+
+    private val routers = mutableListOf<Router>()
 
     fun start() {
         val addr = InetSocketAddress(port.toInt())
@@ -35,16 +45,25 @@ class Server(
                                             println(request.path)
                                             println(request.protocol)
 
-                                            isKeepAlive = isKeepAlive(request.protocol, request.headers)
+                                            val handler = findHandler(request)
+                                            val response = handler?.let {
+                                                val context = Context(request)
+                                                it(context)
+                                            } ?: HttpResponse(
+                                                HttpStatus.NOT_FOUND, ImmutableHttpHeaders(
+                                                    mapOf(
+                                                        "content-length" to listOf("0"),
+                                                        "connection" to listOf("close"),
+                                                    )
+                                                )
+                                            )
 
-                                            socket.write(ByteBuffer.wrap("HTTP/1.1 200 OK\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Content-Length: 13\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Content-Type: text/plain\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Date: Tue Dec 23 01:50:04 JST 2025\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Host: localhost\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Connection: keep-alive\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("\r\n".toByteArray()))
-                                            socket.write(ByteBuffer.wrap("Hello World\r\n".toByteArray()))
+                                            isKeepAlive = isKeepAlive(
+                                                request.protocol,
+                                                request.headers
+                                            ) && !(response.headers["Connection"]?.contains("close") ?: false)
+
+                                            writeResponse(socket, request.protocol, response)
 
                                             if (!isKeepAlive) break
 
@@ -72,10 +91,78 @@ class Server(
         }
     }
 
+    fun addRouter(router: Router) {
+        routers.add(router)
+    }
+
+    private fun findHandler(request: HttpRequest): Handler? {
+        for (router in routers) {
+            val handler = router.match(request)
+            if (handler != null) {
+                return handler
+            }
+        }
+
+        return null
+    }
+
+    // FIXME 無理やり動かしているため、後で直す
+    private fun writeResponse(socket: SocketChannel, protocol: HttpProtocol, response: HttpResponse) {
+        val buffer = ByteBuffer.allocate(2048)
+        val statusLine =
+            "${protocol.version()} ${response.status.code} ${response.status.reasonPhrase}\r\n".toByteArray(Charsets.US_ASCII)
+
+        buffer.put(statusLine)
+        response.headers.forEach { key, values ->
+            values.forEach { value ->
+                val writeLine = "$key: $value\r\n".toByteArray(Charsets.US_ASCII)
+                // FIXME ヘッダ長がバッファより長いと破綻するため、要調整
+                if (buffer.remaining() < writeLine.size) {
+                    buffer.flip()
+                    val written = socket.write(buffer)
+                    if (written < 0) throw IllegalStateException("Unexpected end of stream")
+                    buffer.compact()
+                }
+            }
+        }
+
+        if ("content-type" !in response.headers && response.body.defaultContentType() != null) {
+            val writeLine = "Content-Type: ${response.body.defaultContentType()}\r\n".toByteArray(Charsets.US_ASCII)
+            buffer.put(writeLine)
+        }
+
+        if ("content-length" !in response.headers) {
+            val contentLength = response.body.contentLength()
+            val writeLine = "Content-Length: $contentLength\r\n".toByteArray(Charsets.US_ASCII)
+            buffer.put(writeLine)
+        }
+
+        if ("date" !in response.headers) {
+            val writeLine = "Date: ${
+                ZonedDateTime.now(ZoneId.of("UTC")).format(FIELD_DATE_FORMATTER)
+            }\r\n".toByteArray(Charsets.US_ASCII)
+            buffer.put(writeLine)
+        }
+
+        buffer.put("\r\n".toByteArray(Charsets.US_ASCII))
+        while (buffer.hasRemaining()) {
+            buffer.flip()
+            val written = socket.write(buffer)
+            if (written < 0) throw IllegalStateException("Unexpected end of stream")
+//            buffer.compact()
+        }
+
+        response.body.writeTo(socket)
+    }
+
     private fun isKeepAlive(protocol: HttpProtocol, headers: HttpHeaders): Boolean {
         return when (protocol) {
             HttpProtocol.HTTP_1_1 -> !(headers["Connection"]?.contains("close") ?: false)
             HttpProtocol.HTTP_1_0 -> headers["Connection"]?.contains("keep-alive") == true
         }
+    }
+
+    companion object {
+        private val FIELD_DATE_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME
     }
 }
