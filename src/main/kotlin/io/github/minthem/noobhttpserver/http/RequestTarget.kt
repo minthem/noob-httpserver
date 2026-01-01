5ad15c7b1c9ecbc2fa2cff8bd7aed4df875b07e5
minthem <114272528+minthem@users.noreply.github.com>
2026-01-01 16:38:54 +0900

    Improve `Context` class and `RequestTarget` with decoded path and query handling
    - Add `decodedPath` and `decodedQuery` properties to `RequestTarget` for URI decoding.
    - Extend `Context` to include lazy initialization of `path` and `queryParams`.

--- src/main/kotlin/io/github/minthem/noobhttpserver/http/RequestTarget.kt
+++ src/main/kotlin/io/github/minthem/noobhttpserver/http/RequestTarget.kt
@@ -12,4 +12,19 @@ data class RequestTarget internal constructor(private val value: String) {
     val rawPath = pathQuery.first.ifEmpty { "/" }
 
     val rawQuery = pathQuery.second
+
+    val decodedPath: String by lazy { UriDecoder.decodePath(rawPath) }
+
+    val decodedQuery: Map<String, List<String>> by lazy { decodedQuery() }
+
+    private fun decodedQuery(): Map<String, List<String>> {
+        val queries = rawQuery?.split('&') ?: emptyList()
+
+        return queries.filter { it.isNotBlank() }.map {
+            val pair = it.split('=', limit = 2)
+            val decKey = UriDecoder.decodeQuery(pair[0])
+            val decValue = pair.getOrNull(1)?.let { v -> UriDecoder.decodeQuery(v) } ?: ""
+            decKey to decValue
+        }.groupBy({ it.first }, { it.second })
+    }
 }
