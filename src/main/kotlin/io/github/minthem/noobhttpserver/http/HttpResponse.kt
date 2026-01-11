bebdebdec3084324e04fe7b5869c68a15a76d357
minthem <114272528+minthem@users.noreply.github.com>
2026-01-12 01:36:48 +0900

    Add support for chunked string body with configurable charset

--- src/main/kotlin/io/github/minthem/noobhttpserver/http/HttpResponse.kt
+++ src/main/kotlin/io/github/minthem/noobhttpserver/http/HttpResponse.kt
@@ -24,6 +24,9 @@ class HttpResponse private constructor(
         fun body(bytes: ByteArray) = apply { body = BodySpec.Binary(bytes) }
         fun body(path: Path, charset: Charset = Charsets.UTF_8) = apply { body = BodySpec.File(path, charset) }
         fun body(source: CloseableSequence<ByteArray>) = apply { body = BodySpec.Chunked(source) }
+        fun body(source: CloseableSequence<String>, charset: Charset = Charsets.UTF_8) = apply {
+            body = BodySpec.Chunked(source.map { it.toByteArray(charset) }.asCloseable { source.close() })
+        }
 
         fun build(): HttpResponse {
             val executor = BodyWriteExecutorFactory.create(body)
